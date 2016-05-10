c393f7857961ad1e8a39b4d14e479a32a11f3f31
trikitrok <trikitrok@gmail.com>
2016-05-10 16:20:44 +0200

    refactor: changed tests descriptions

--- test/gildedRoseSpec.js
+++ test/gildedRoseSpec.js
@@ -4,8 +4,8 @@ var GildedRose = require('../src/gildedRose.js'),
   Inventory = GildedRose.Inventory,
   Item = GildedRose.Item;
 
-describe("GildedRose inventory", function() {
-  it("at the end of each day the quality of a product decreases by 1", function() {
+describe("GildedRose inventory at the end of each day", function() {
+  it("the quality of a product decreases by 1", function() {
     var anyInitialQuality = 10,
       notNeededDaysToSell = null,
       item = new Item("A regular item", notNeededDaysToSell, anyInitialQuality),
@@ -16,7 +16,7 @@ describe("GildedRose inventory", function() {
     expect(item.quality).toBe(anyInitialQuality - 1);
   });
 
-  it("at the end of each day the days to be sold of a product decrease by 1", function() {
+  it("the days to be sold of a product decrease by 1", function() {
     var anyInitialdaysToBeSold = 4,
       notNeededQuality = null,
       item = new Item("A regular item", anyInitialdaysToBeSold, notNeededQuality),

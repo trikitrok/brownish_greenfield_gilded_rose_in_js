c7b3783b95c4bcd691fb475224f0510833a312af
trikitrok <trikitrok@gmail.com>
2016-05-10 17:10:57 +0200

    refactor: extracting degradable item creation and types to a separatte module DegradableItem

---
+++ src/degradableItem.js
@@ -0,0 +1,38 @@
+module.exports = {
+  create: create
+};
+
+function create(item) {
+  if(item.name == "Aged Brie") {
+    return AgedBrie(item);
+  }
+  return RegularItem(item);
+}
+
+function RegularItem(item) {
+  return {
+    update: update
+  };
+
+  function update() {
+    item.sell_in -= 1;
+    if(item.quality > 0) {
+      if(item.sell_in >= 0) {
+        item.quality -= 1;  
+      } else {
+        item.quality -= 2;  
+      }
+    } 
+  }
+}
+
+function AgedBrie(item) {
+  return {
+    update: update
+  };
+
+  function update() {
+    item.sell_in -= 1;
+    item.quality += 1;
+  }
+}
--- src/degradableItems.js
+++ src/degradableItems.js
@@ -1,56 +1,25 @@
+var DegradableItem = require('./degradableItem.js');
+
 module.exports = {
   createFrom: createFrom
 };
 
 function createFrom(items) {
-  return DegradableItems(items.map(createItem));
+  return DegradableItems(
+    items.map(DegradableItem.create)
+  );
 }
 
-function createItem(item) {
-  if(item.name == "Aged Brie") {
-    return AgedBrie(item);
-  }
-  return RegularItem(item);
-}
-
-function DegradableItems(items) {
+function DegradableItems(degradableItems) {
   return {
     update: update
   };
 
   function update() {
-    items.forEach(
+    degradableItems.forEach(
       function(item) {
         item.update();
       }
     );
-  }
-}
-
-function RegularItem(item) {
-  return {
-    update: update
-  };
-
-  function update() {
-    item.sell_in -= 1;
-    if(item.quality > 0) {
-      if(item.sell_in >= 0) {
-        item.quality -= 1;  
-      } else {
-        item.quality -= 2;  
-      }
-    } 
-  }
-}
-
-function AgedBrie(item) {
-  return {
-    update: update
-  };
-
-  function update() {
-    item.sell_in -= 1;
-    item.quality += 1;
   }
 }

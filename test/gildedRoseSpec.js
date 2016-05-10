98c65ccc0a4eda92fc8b2103c097d67cb366b5e0
trikitrok <trikitrok@gmail.com>
2016-05-10 16:35:58 +0200

    refactor: rename some variables in tests

--- test/gildedRoseSpec.js
+++ test/gildedRoseSpec.js
@@ -16,19 +16,18 @@ describe("GildedRose inventory at the end of each day", function() {
   });
 
   it("the days to be sold of a product decrease by 1", function() {
-    var anyInitialdaysToBeSold = 4,
-      notNeededQuality = null,
-      item = new Item("A regular item", anyInitialdaysToBeSold, notNeededQuality),
+    var daysToBeSold = 4, notNeededQuality = null,
+      item = new Item("A regular item", daysToBeSold, notNeededQuality),
       inventory = Inventory([item]);
 
     inventory.update();
 
-    expect(item.sell_in).toBe(anyInitialdaysToBeSold - 1);
+    expect(item.sell_in).toBe(daysToBeSold - 1);
   });
 
   it("the Quality of an item is never negative", function() {
-    var notNeededDaysToSell = null,
-      item = new Item("A regular item", notNeededDaysToSell, 0),
+    var daysToBeSold = 8, quality = 0,
+      item = new Item("A regular item", daysToBeSold, quality),
       inventory = Inventory([item]);
 
     inventory.update();   

bada6d34ab3465fa181e53fb78a8a2fdd24bf706
trikitrok <trikitrok@gmail.com>
2016-05-10 16:52:43 +0200

    refactor: separate creation and update of degradable items

--- src/gildedRose.js
+++ src/gildedRose.js
@@ -5,49 +5,57 @@ module.exports = {
   Item: Item
 };
 
+var DegradableItem = {
+  create: create
+};
+
+function create(item) {
+  if(item.name == "Aged Brie") {
+    return AgedBrie(item);
+  }
+  return RegularItem(item);
+}
+
+function RegularItem(item) {
+  return {
+    update: update
+  };
+
+  function update() {
+    item.sell_in -= 1;
+    if(item.quality > 0) {
+      if(item.sell_in >= 0) {
+        item.quality -= 1;  
+      } else {
+        item.quality -= 2;  
+      }
+    } 
+  }
+}
+
+function AgedBrie(item) {
+  return {
+    update: update
+  };
+
+  function update() {
+    item.sell_in -= 1;
+    item.quality += 1;
+  }
+}
+
 function Inventory(items) {
   return {
     update: update
   };
 
   function update() {
-    items.forEach(updateItem);
+    var degradableItems = items.map(DegradableItem.create);
+    degradableItems.forEach(updateItem);
   }
 
-  function updateItem(item) {
-    if(item.name == "Aged Brie") {
-      AgedBrie(item).update();
-      return;
-    }
-    RegularItem(item).update();
-  }
-
-  function RegularItem(item) {
-    return {
-      update: update
-    };
-
-    function update() {
-      item.sell_in -= 1;
-      if(item.quality > 0) {
-        if(item.sell_in >= 0) {
-          item.quality -= 1;  
-        } else {
-          item.quality -= 2;  
-        }
-      } 
-    }
-  }
-
-  function AgedBrie(item) {
-    return {
-      update: update
-    };
-
-    function update() {
-      item.sell_in -= 1;
-      item.quality += 1;
-    }
+  function updateItem(degradableItem) {
+    degradableItem.update();
   }
 }
 

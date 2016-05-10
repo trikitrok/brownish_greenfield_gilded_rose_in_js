80cf846cae90d3305efb7328ab780a21437014de
trikitrok <trikitrok@gmail.com>
2016-05-10 16:46:29 +0200

    refactor: introduced factory functions for both types of Items
    They are wrappers for Item

--- src/gildedRose.js
+++ src/gildedRose.js
@@ -15,18 +15,39 @@ function Inventory(items) {
   }
 
   function updateItem(item) {
-    item.sell_in -= 1;
-    if(item.quality > 0) {
-      if(item.name == "Aged Brie") {
-        item.quality += 1;
-        return;
-      }
-      if(item.sell_in >= 0) {
-        item.quality -= 1;  
-      } else {
-        item.quality -= 2;  
-      }
-    }      
+    if(item.name == "Aged Brie") {
+      AgedBrie(item).update();
+      return;
+    }
+    RegularItem(item).update();
+  }
+
+  function RegularItem(item) {
+    return {
+      update: update
+    };
+
+    function update() {
+      item.sell_in -= 1;
+      if(item.quality > 0) {
+        if(item.sell_in >= 0) {
+          item.quality -= 1;  
+        } else {
+          item.quality -= 2;  
+        }
+      } 
+    }
+  }
+
+  function AgedBrie(item) {
+    return {
+      update: update
+    };
+
+    function update() {
+      item.sell_in -= 1;
+      item.quality += 1;
+    }
   }
 }
 
